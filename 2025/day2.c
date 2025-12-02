78428e377aef07959f42222211a877d909a9a6c7
bittersweetpuff <biszkoptex@gmail.com>
2025-12-02 16:14:07 +0100

    Day 2 Puzzle 2

--- 2025/day2.c
+++ 2025/day2.c
@@ -7,6 +7,22 @@
  #include <string.h>
 
 
+ int check_pattern_repeat(long int pattern, long int number, int pattern_lenght)
+ {
+     long int multi_pattern = pattern;
+     while (multi_pattern<=number)
+     {
+         //printf("%ld\n", multi_pattern);
+         if (multi_pattern == number)
+         {
+             return 1;
+         }
+         multi_pattern = (multi_pattern*(long)pow(10, pattern_lenght)) + pattern;
+     }
+
+     return 0;
+ }
+
  int check_id_validity(long int id)
  {
      int length = 1;
@@ -31,19 +47,56 @@
      }
  }
 
- long long int scan_range(long int start, long int stop)
+ int check_id_validity_part2(long int id)
+ {
+     int length = 1;
+     while(id%(long)pow(10, length) != id)
+         length+=1;
+
+     for(int pattern_lenght = 1; pattern_lenght<=length/2; pattern_lenght += 1)
+     {
+         long int pattern = id/(long)pow(10, length-pattern_lenght);
+         if(check_pattern_repeat(pattern, id, pattern_lenght))
+         {
+             printf("MULTIPATERN: %ld\n", id);
+             return 0;
+         }
+
+
+     }
+
+     return 1;
+
+ }
+
+
+ long long int scan_range(long int start, long int stop, int method)
  {
      long long int sum = 0;
+     long long int sum2 = 0;
      for (long int i = start; i <= stop; i += 1)
      {
+         //printf("Checking: %ld\n", i);
          /* add ID if invalid */
          if(!check_id_validity(i))
          {
              //printf("%ld\n", i);
              sum += i;
          }
+         if(!check_id_validity_part2(i))
+         {
+             sum2 += i;
+         }
      }
-     return sum;
+     if (method == 1)
+     {
+         return sum;
+     }
+     else
+     {
+         return sum2;
+     }
+
  }
 
 
@@ -73,10 +126,10 @@
            if (sscanf(range, "%ld-%ld", &start, &stop))
            {
                //printf("%ld-%ld\n", start, stop);
-               sumOfInvalids += scan_range(start, stop);
+               sumOfInvalids += scan_range(start, stop, 2);
            }
            range = strtok(NULL, ",");
     }
-     printf("The sum of all invalid IDs is: %lld\n", sumOfInvalids);
+     printf("The sum of all invalid IDs (second method) is: %lld\n", sumOfInvalids);
      return 0;
  }
