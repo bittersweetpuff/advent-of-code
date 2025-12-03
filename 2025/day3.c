eb36277765f15a46a91514874ab2b10320fdff19
bittersweetpuff <biszkoptex@gmail.com>
2025-12-03 08:46:10 +0100

    Day 2 Puzzle 2 solution

--- 2025/day3.c
+++ 2025/day3.c
@@ -4,14 +4,32 @@
  */
  #include <stdio.h>
 
- int get_max_joltage(char* bank)
+ long int get_max_joltage(char* bank, int batteries_needed)
  {
     printf("%s", bank);
-    int first_battery = -1;
-    int first_battery_index = -1;
-    int second_battery = -1;
+    long int joltage = 0;
+    int max_joltage_battery = -1;
+    int last_bettery_index = -1;
     /* Finding first battery */
     /* Last char before null sigh is newline, so I iterate to that */
+
+    for(int j = 1; j <= batteries_needed; j+=1)
+    {
+        for(int i = last_bettery_index+1; bank[i + (batteries_needed - j)] != '\n'; i += 1)
+        {
+            int current_battery = bank[i] - '0';
+            if(current_battery>max_joltage_battery)
+            {
+                max_joltage_battery = current_battery;
+                last_bettery_index = i;
+            }
+        }
+        printf("Max Battery [%d]: %d\n", j, max_joltage_battery);
+        joltage = (joltage * 10) + max_joltage_battery;
+        max_joltage_battery = -1;
+    }
+    /*
+
     for(int i = 0; bank[i+1] != '\n'; i+=1)
     {
         int current_battery = bank[i] - '0';
@@ -25,7 +43,7 @@
     }
     printf("\nFirst Battery: %d\n", first_battery);
 
-    /* Finding second battery */
+
     for(int i = first_battery_index+1; bank[i]!='\n'; i += 1 )
     {
         int current_battery = bank[i] - '0';
@@ -37,8 +55,9 @@
         printf("%d ", current_battery);
     }
     printf("\nSecond Battery: %d\n", second_battery);
-    int joltage = (first_battery * 10) + second_battery;
-    printf("Joltage: %d", joltage);
+    */
+    printf("Joltage: %ld", joltage);
+
 
 
     printf("\n");
@@ -48,7 +67,7 @@
 
  int main(int argc, char *argv[])
  {
-     int joltage_sum = 0;
+     long int joltage_sum = 0;
      /* Create pointer to the file */
      FILE *fptr;
      /*  Open a file in read mode */
@@ -58,11 +77,11 @@
 
      while(fgets(inputString, 500, fptr))
      {
-         joltage_sum += get_max_joltage(inputString);
+         joltage_sum += get_max_joltage(inputString, 12);
      }
      fclose(fptr);
 
-     printf("Total output joltage is: %d\n", joltage_sum);
+     printf("Total output joltage is: %ld\n", joltage_sum);
 
      return 0;
  }
