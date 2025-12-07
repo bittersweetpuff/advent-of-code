186c79bf2afa3150a40804200bc8ab28646c3632
bittersweetpuff <biszkoptex@gmail.com>
2025-12-07 11:37:28 +0100

    Day 7 Puzzle 2 Solution

--- 2025/day7.c
+++ 2025/day7.c
@@ -3,7 +3,12 @@
  * https://adventofcode.com/2025/day/7
  */
  #include <stdio.h>
-#include <string.h>
+ #include <string.h>
+
+ /* Global Variables lets goooo! */
+ char tachyon_manifold[200][500];
+ char quantum_tachyon_manifold[200][500];
+ int level_count = 0;
 
  void print_tachyon_manifold(char mainfold[200][500], int size)
  {
@@ -11,6 +16,43 @@
      {
          printf("%s", mainfold[i]);
      }
+ }
+
+ void print_timeline_matrix(int matrix[200][500], int rows, int cols)
+ {
+     for(int i = 0; i<rows; i+=1)
+     {
+         for (int j = 0; j < rows; j+= 1)
+         {
+             printf(" %d", matrix[i][j]);
+         }
+         printf("\n");
+     }
+ }
+
+ long int get_timelines(long int timeline_matrix[200][500], int row, int column)
+ {
+     long int result;
+     if(row == level_count - 1)
+     {
+         timeline_matrix[row][column] = 1;
+         return 1;
+     }
+     if(timeline_matrix[row][column] != 0)
+     {
+         return timeline_matrix[row][column];
+     }
+     if(quantum_tachyon_manifold[row+1][column] == '.')
+     {
+         result = get_timelines(timeline_matrix, row+1, column);
+     }
+     if(quantum_tachyon_manifold[row+1][column] == '^')
+     {
+         result = get_timelines(timeline_matrix, row+1, column-1) + get_timelines(timeline_matrix, row+1, column+1);
+     }
+     timeline_matrix[row][column] = result;
+     return result;
+
  }
 
  int process_and_count(char mainfold[200][500], int size, int print)
@@ -42,17 +84,35 @@
              print_tachyon_manifold(mainfold, size);
          }
      }
-
-
      return splits;
+ }
 
+ long int qunatum_process_and_count()
+ {
+     long int timelines = 0;
+     long int timeline_matrix[200][500];
+     /* Zero the matrix */
+     for(int i = 0; i < 200; i += 1)
+         for(int j = 0; j < 500; j++)
+             timeline_matrix[i][j] = 0;
+
+     int r = 0;
+     int c = 0;
+     for(int i = 0; quantum_tachyon_manifold[r][i] != '\n'; i += 1)
+     {
+         if(quantum_tachyon_manifold[r][i] == 'S')
+         {
+             c = i;
+         }
+     }
+
+     timelines = get_timelines(timeline_matrix, r, c);
+
+     return timelines;
  }
 
  int main(int argc, char *arcv[])
  {
-     char tachyon_manifold[200][500];
-     int level_count = 0;
-
      /* Create pointer to the file */
      FILE *fptr;
      /*  Open a file in read mode */
@@ -62,13 +122,16 @@
      while(fgets(inputString, 500, fptr))
      {
          strcpy(tachyon_manifold[level_count], inputString);
+         strcpy(quantum_tachyon_manifold[level_count], inputString);
          level_count += 1;
      }
      /* Close file */
      fclose(fptr);
 
      //print_tachyon_manifold(tachyon_manifold, level_count);
-     int splits = process_and_count(tachyon_manifold, level_count, 1);
+     int splits = process_and_count(tachyon_manifold, level_count, 0);
+     long int timelines = qunatum_process_and_count();
      printf("Number of times beam splits: %d\n", splits);
+     printf("Number of timelines created: %ld\n", timelines);
      return 0;
  }
